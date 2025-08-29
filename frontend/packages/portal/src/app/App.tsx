3e147a0adc973381b618e6b869c803efda6927b5
denis <denis.beresnev@is.ru>
2025-08-29 12:44:18 +0300

    feat: added portal example to monorepo

---
+++ frontend/packages/portal/src/app/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Button } from '@frontend/shared/shared/ui/Button';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Portal</h1>
+      <div>
+      <Button>Shared Button</Button>
+      <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
